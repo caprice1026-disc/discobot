1b064ece9658957cedacec5c040da792d5abd834
caprice_dev <108645246+caprice1026-disc@users.noreply.github.com>
2022-12-30 23:44:41 +0900

    Add files via upload

---
+++ ディスココマンド（新）.py
@@ -0,0 +1,21 @@
+import discord
+from discord.ext import commands
+import asyncio
+#asynicoがいるのかは知らん！！　多分使うやろ！
+
+# Botのアクセストークン書け！
+TOKEN = ''
+#プレフィックス指定　アクティビティーのとこが許されるのかは不明
+bot = commands.Bot(activity=discord.Game("人生"))
+
+#インテントの設定（ディベロッパーポータルの方でも変えないと動かんぞ！！）
+intents = discord.Intents.default()
+intents.members = True
+intents.message_content = True
+
+#コマンド書いてくよ～
+@bot.command()
+async def promotion(ctx,prm,…):
+
+
+#これを参考にしろ！https://zenn.dev/yuzmidori/articles/7a60b24f1ace4f
