e635316120fa48ee3ab7fe0f3e38ba48faac6227
caprice_dev <108645246+caprice1026-disc@users.noreply.github.com>
2023-02-11 17:28:44 +0900

    Add files via upload

--- ディスココマンド（新）.py
+++ ディスココマンド（新）.py
@@ -1,22 +1,46 @@
 import discord
 from discord.ext import commands
-import asyncio
-#asynicoがいるのかは知らん！！　多分使うやろ！
+from discord.utils import get
 
 # Botのアクセストークン書け！
 TOKEN = ''
-#プレフィックス指定　アクティビティーのとこが許されるのかは不明
-bot = commands.Bot(activity=discord.Game("人生"))
+#アクティビティーのとこが許されるのかは不明
+bot = commands.Bot(command_prefix="/" intents=intents, activity=discord.Game("ゴミみてえな人生"))
 
 #インテントの設定（ディベロッパーポータルの方でも変えないと動かんぞ！！）
 intents = discord.Intents.default()
 intents.members = True
 intents.message_content = True
 
-#コマンド書いてくよ～
+#bot起動時のイベント
+@bot.event
+async def on_ready():
+ print('We have logged in as {0.user}'.format(bot))
+
+#コマンドのヤツ difだっけasyncいるっけ
+def is_creator(ctx):
+    async def predicate(ctx):
+        return ctx.role.roleID > role.roleID
+    return commands.check(predicate)
+
 @bot.command()
-async def promotion(ctx,prm,…):
+@commands.check(is_creator)
+async def promotion(ctx, *, arg):
+  await.send(message.content)
+
+ 
+#こっから先を最適化target_channel = get(message.guild.channels, name='転送先のチャンネル名')
+ #await target_channel.send(message.content)
+# if message.author.guild_permissions.manage_messages:
+        # 特定のロールを持つユーザーである場合の処理を記述する
+   #    
+   #     await target_channel.send(message.content)
 
 
 #これを参考にしろ！https://zenn.dev/yuzmidori/articles/7a60b24f1ace4f
-#エクステンションの方を使って書いてくれ　確実に楽だから　pyのdocsとごっちゃになりやすいので注意　https://discordpy.readthedocs.io/ja/latest/ext/commands/index.html
+#これ使え！！　https://discordpy.readthedocs.io/ja/latest/ext/commands/index.html
+# https://discordpy.readthedocs.io/ja/latest/api.html#discord.Intents.message_content
+
+#cliantを全部botに書き換えていけ
+
+
